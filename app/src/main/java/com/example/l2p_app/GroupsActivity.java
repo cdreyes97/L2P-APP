05619b3efb55fa75cd1e6c20d2edc2d62f131a90
creyesl <creyesl@larrainvial.com>
2021-05-18 19:20:37 -0400

    firebase database en build.gradle

---
+++ app/src/main/java/com/example/l2p_app/GroupsActivity.java
@@ -0,0 +1,11 @@
+package com.example.l2p_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DatabaseReference;
+
+public class GroupsActivity extends AppCompatActivity {
+
+    private DatabaseReference db;
+
+}
