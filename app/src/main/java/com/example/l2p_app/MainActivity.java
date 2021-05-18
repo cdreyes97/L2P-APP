e88affeff55f6cafbf8a17c266aa595b44a074ad
Camilo Parra <caparra@alumnos.inf.utfsm.cl>
2021-05-18 18:22:56 -0400

    implemented basic array adapter

--- app/src/main/java/com/example/l2p_app/MainActivity.java
+++ app/src/main/java/com/example/l2p_app/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.l2p_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    String[] COUNTRIES = { "Argentina", "Bolivia","Chile"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayAdapter adaptador = new ArrayAdapter(this, R.layout.landing_activity, COUNTRIES);
+
+        ListView gamelist = findViewById(R.id.gamelist);
+
+        gamelist.setAdapter(adaptador);
     }
 }
