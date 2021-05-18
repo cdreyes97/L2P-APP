d2797d0a0a3d9dc31e5e6dd87c2b712d802756df
creyesl <creyesl@larrainvial.com>
2021-05-18 19:36:08 -0400

    models and adapter packages

---
+++ app/src/main/java/com/example/l2p_app/adapters/RoomAdapter.java
@@ -0,0 +1,8 @@
+package com.example.l2p_app.adapters;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RoomAdapter extends RecyclerView.Adapter {
+
+    
+}
---
+++ app/src/main/java/com/example/l2p_app/models/Room.java
@@ -0,0 +1,43 @@
+package com.example.l2p_app.models;
+
+public class Room {
+
+    private String name;
+    private String game;
+    private int roomCapacity;
+    private int capacityUsed;
+
+    public Room() {}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGame() {
+        return game;
+    }
+
+    public void setGame(String game) {
+        this.game = game;
+    }
+
+    public int getRoomCapacity() {
+        return roomCapacity;
+    }
+
+    public void setRoomCapacity(int roomCapacity) {
+        this.roomCapacity = roomCapacity;
+    }
+
+    public int getCapacityUsed() {
+        return capacityUsed;
+    }
+
+    public void setCapacityUsed(int capacityUsed) {
+        this.capacityUsed = capacityUsed;
+    }
+}
