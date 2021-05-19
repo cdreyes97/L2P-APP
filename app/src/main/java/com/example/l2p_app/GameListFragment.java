34d488f46a9251d2a39fcc17d9964c96c50599dc
Camilo Parra <caparra@alumnos.inf.utfsm.cl>
2021-05-18 21:54:18 -0400

    se agregaron las imagenes de ejemplo para la lista de juegos y primera imagen clickeable

---
+++ app/src/main/java/com/example/l2p_app/GameListFragment.java
@@ -0,0 +1,55 @@
+package com.example.l2p_app;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.example.l2p_app.databinding.GameListBinding;
+
+public class GameListFragment extends Fragment {
+
+    private GameListBinding binding;
+
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+
+        binding = GameListBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.buttonFirst.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(GameListFragment.this)
+                        .navigate(R.id.action_FirstFragment_to_SecondFragment);
+            }
+        });
+    }
+
+
+
+
+
+
+
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
